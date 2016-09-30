173b43f7fc24e977d35e5de4e2b30781b98551d4
Owner <Owner@JennySC>
2016-09-29 18:40:06 -0600

    Todos los CRUD's

---
+++ Guia06_POO2_JennyferSoto/src/main/java/com/sv/udb/controlador/GruposBean.java
@@ -0,0 +1,165 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sv.udb.controlador;
+
+import com.sv.udb.ejb.GruposFacadeLocal;
+import com.sv.udb.modelo.Grupos;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+import org.primefaces.context.RequestContext;
+
+/**
+ *
+ * @author Owner
+ */
+@Named(value = "gruposBean")
+@ViewScoped
+public class GruposBean implements Serializable {
+
+    @EJB
+    private GruposFacadeLocal FCDEGrup;
+    private Grupos objeGrup;
+    private List<Grupos> listGrup;
+    private boolean guardar;
+
+    public Grupos getObjeGrup() {
+        return objeGrup;
+    }
+
+    public void setObjeGrup(Grupos objeGrup) {
+        this.objeGrup = objeGrup;
+    }
+
+    public List<Grupos> getListGrup() {
+        return listGrup;
+    }
+
+    public boolean isGuardar() {
+        return guardar;
+    }
+    
+    
+    /**
+     * Creates a new instance of GruposBean
+     */
+    public GruposBean() {
+    }
+    
+    @PostConstruct
+    public void init()
+    {
+        this.limpForm();
+        this.consTodo();
+    }
+    
+    public void limpForm()
+    {
+        this.objeGrup = new Grupos();
+        this.guardar = true;        
+    }
+    
+    public void guar()
+    {
+        RequestContext ctx = RequestContext.getCurrentInstance(); //Capturo el contexto de la página
+        try
+        {
+            FCDEGrup.create(this.objeGrup);
+            this.listGrup.add(this.objeGrup);
+            this.limpForm();
+            ctx.execute("setMessage('MESS_SUCC', 'Atención', 'Datos guardados')");
+        }
+        catch(Exception ex)
+        {
+            ctx.execute("setMessage('MESS_ERRO', 'Atención', 'Error al guardar ')");
+        }
+        finally
+        {
+            
+        }
+    }
+    
+    public void modi()
+    {
+        RequestContext ctx = RequestContext.getCurrentInstance(); //Capturo el contexto de la página
+        try
+        {
+            this.listGrup.remove(this.objeGrup); //Limpia el objeto viejo
+            FCDEGrup.edit(this.objeGrup);
+            this.listGrup.add(this.objeGrup); //Agrega el objeto modificado
+            ctx.execute("setMessage('MESS_SUCC', 'Atención', 'Datos Modificados')");
+        }
+        catch(Exception ex)
+        {
+            ctx.execute("setMessage('MESS_ERRO', 'Atención', 'Error al modificar ')");
+        }
+        finally
+        {
+            
+        }
+    }
+    
+    public void elim()
+    {
+        RequestContext ctx = RequestContext.getCurrentInstance(); //Capturo el contexto de la página
+        try
+        {
+            FCDEGrup.remove(this.objeGrup);
+            this.listGrup.remove(this.objeGrup);
+            this.limpForm();
+            ctx.execute("setMessage('MESS_SUCC', 'Atención', 'Datos Eliminados')");
+        }
+        catch(Exception ex)
+        {
+            ctx.execute("setMessage('MESS_ERRO', 'Atención', 'Error al eliminar')");
+        }
+        finally
+        {
+            
+        }
+    }
+    
+    public void consTodo()
+    {
+        try
+        {
+            this.listGrup = FCDEGrup.findAll();
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            
+        }
+    }
+    
+    public void cons()
+    {
+        RequestContext ctx = RequestContext.getCurrentInstance(); //Capturo el contexto de la página
+        int codi = Integer.parseInt(FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("codiGrupPara"));
+        try
+        {
+            this.objeGrup = FCDEGrup.find(codi);
+            this.guardar = false;
+            ctx.execute("setMessage('MESS_SUCC', 'Atención', 'Consultado a " + 
+                    String.format("%s", this.objeGrup.getNombGrup()) + "')");
+        }
+        catch(Exception ex)
+        {
+            ctx.execute("setMessage('MESS_ERRO', 'Atención', 'Error al consultar')");
+        }
+        finally
+        {
+            
+        }
+    }
+}
